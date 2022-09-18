0ae70c1f2adc360dd7775561b640a6d629598316
Moises Cruz <moisesbraga223@gmail.com>
2022-09-18 14:48:44 -0300

    playlist

--- src/components/Playlist/ContentMusic.tsx
+++ src/components/Playlist/ContentMusic.tsx
@@ -1,22 +1,27 @@
-import { Box, Grid, Text } from "@chakra-ui/react";
+import { Box, Grid, Text, Image, Flex, Link } from "@chakra-ui/react";
 
 interface DefaultMusicProps {
     numb: string;
     music: string,
     artist: string,
     album: string,
+    addIn: string,
     time: string
 }
 
-export function ContentMusic({numb, music, artist, album, time}: DefaultMusicProps) {
+export function ContentMusic({numb, music, artist, album, addIn, time}: DefaultMusicProps) {
     return (
         <>
-            <Grid padding="0 16px" alignItems="center" gridTemplateColumns="[index] 16px [first] 4fr [last]minmax(120px,1fr)" gap="16px">
+            <Grid padding="0 16px" alignItems="center" gridTemplateColumns= "[index] 16px [first] 6fr [var1] 4fr [var2] 3fr [last] minmax(120px,1fr)" gap="16px">
                 <Text color="#b3b3b3" display="block" fontSize="16px" fontWeight="bold">{numb}</Text>
                 <Box>
                     <Text fontWeight="400" color="white">{music}</Text>
-                    <Text color="#b3b3b3">{artist} | {album}</Text>
+                    <Flex alignItems="center" flexDirection="row">
+                        <Text fontSize="14px" color="#b3b3b3"><Link href="/playlist">{artist}</Link></Text>
+                    </Flex>
                 </Box>
+                <Text fontSize="14px" color="#b3b3b3"><Link href="/playlist">{album}</Link></Text>
+                <Text fontSize="14px" color="#b3b3b3">{addIn}</Text>
                 <Text color="#b3b3b3" justifySelf="end">{time}</Text>
             </Grid>
         </>
--- src/components/Playlist/Main.tsx
+++ src/components/Playlist/Main.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Image, Grid } from "@chakra-ui/react";
 import { NavBarLeft } from "../Common/NavBarLeft";
 import { ContentMusic } from "./ContentMusic";
 import { DefaultImage } from "./DefaultImage";
@@ -34,22 +34,38 @@ export function Main() {
               </Flex>
             </Box>
           </Box>
+          <Box backgroundColor="black" width="100%" padding="24px 32px">
+            <Flex gap="16px" alignItems="center">
+              <Image
+                src="/assets/icons/playIcon.svg"
+                alt="Ícone de play"
+                marginRight="24px"
+                _hover={{ transform: "scale(1.04)" }}
+              />
+              <Image src="/assets/icons/optionsICon.svg" alt="Ícone de opções" />
+            </Flex>
+          </Box>
           <Box background="#121212" padding="32px 32px">
-            <Flex
+            <Grid gridTemplateColumns= "[index] 16px [first] 6fr [var1] 4fr [var2] 3fr [last] minmax(120px,1fr);"
               padding="0 16px"
+              alignItems="center"
               color="#b3b3b3"
               marginBottom="32px"
               gap="16px"
               borderBottom="#fff"
             >
               <Text>#</Text>
-              <Text textTransform="uppercase">Título</Text>
-            </Flex>
+              <Text fontSize="12px" textTransform="uppercase">Título</Text>
+              <Text fontSize="12px" textTransform="uppercase">Álbum</Text>
+              <Text fontSize="12px" textTransform="uppercase">Adicionado em</Text>
+              <Image justifySelf="end" width="21px" src="/assets/icons/timeIcon.svg" alt="Ícone de um relogio" />
+            </Grid>
             <ContentMusic
               numb="1"
               music={"Cap Way"}
               artist={"Yunk Vino"}
               album={"237"}
+              addIn={"13 de fev. de 2022"}
               time={"2:56"}
             ></ContentMusic>
           </Box>
