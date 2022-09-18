608d4d4568212d1eaa9fa67c38f7c2cd509da06d
Moises Cruz <moisesbraga223@gmail.com>
2022-09-18 01:10:57 -0300

    playlist

---
+++ src/components/Common/NavBarLeft.tsx
@@ -0,0 +1,45 @@
+import { Box, Flex, Image, Link, Text } from "@chakra-ui/react";
+import { Logo } from "./Logo";
+
+export function NavBarLeft() {
+  return (
+    <Box
+      backgroundColor="#000"
+      minWidth="241px"
+      paddingTop="15px"
+      zIndex="3"
+      position="relative"
+      height="100vh"
+    >
+      <Flex flexDirection="column">
+        <Flex>
+          <Logo />
+        </Flex>
+        <Box>
+            <NavBarLeftItems src={"assets/icons/homeIcon.svg"} alt={"Ícone do home"} text={"Início"}></NavBarLeftItems>
+            <NavBarLeftItems src={"assets/icons/searchIcon.svg"} alt={"Ícone da lupa de pesquisa"} text={"Buscar"}></NavBarLeftItems>
+            <NavBarLeftItems src={"assets/icons/library.svg"} alt={"Ícone do home"} text={"Sua Biblioteca"}></NavBarLeftItems>
+        </Box>
+
+        <Box paddingTop="24px">
+            <NavBarLeftItems src={"assets/icons/moreIcon.svg"} alt={"Icone de adicionar mais"} text={"Criar Playlist"}></NavBarLeftItems>
+            <NavBarLeftItems src={"assets/icons/heartIcon.svg"} alt={"Icone de da opção de músicas curtidas"} text={"Músicas Curtidas"}></NavBarLeftItems>
+        </Box>
+
+      </Flex>
+    </Box>
+  );
+}
+
+const NavBarLeftItems = ({src, alt, text}) => {
+    return (
+        <>
+        <Link display="flex" alignItems="center" flexDirection="row" gap="16px" padding="18px 36px 0 36px" height="40px" href="/playlist" color="#C7C7C7"  _hover={{ color: 'white' }}>
+        <Image src={src} alt={alt} />
+        <Text fontSize="14px" fontWeight="bold">
+            {text}
+        </Text>
+        </Link>
+        </>
+    );
+}
--- src/components/Playlist/Main.tsx
+++ src/components/Playlist/Main.tsx
@@ -1,34 +1,60 @@
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { NavBarLeft } from "../Common/NavBarLeft";
 import { ContentMusic } from "./ContentMusic";
 import { DefaultImage } from "./DefaultImage";
 
 export function Main() {
-    return (
-        <>
-            <Flex>
-                <Box width="250px" height="100vh"></Box>
-                <Box width="100%" maxHeight="500px" minHeight="340px">
-                    <Box background="linear-gradient(transparent 0, rgba(0,0,0.5) 100%) #fff">
-                        <Box padding="86px 32px 24px" max-width="100%" backgroundColor="rgb(32, 72, 48)">
-                            <Flex>
-                                <DefaultImage src={"/assets/playlist/playlistImage.svg"} alt={"Imagem da playlist: o drip"}></DefaultImage>
-                                <Box color="white" width="auto" margin="24px 0 0 24px">
-                                    <Text textTransform="uppercase" paddingTop="10px">playlist</Text>
-                                    <Heading margin="8px 0 12px" fontSize="98px" fontWeight="900">O Drip</Heading>
-                                    <Text>Chique e confortável como a realeza do Trap. Foto: Brandão85</Text>
-                                </Box>
-                            </Flex>
-                        </Box>
-                    </Box>
-                    <Box padding="0 32px">
-                        <Flex padding="0 16px" color="#b3b3b3" marginBottom="32px" gap="16px">
-                            <Text>#</Text>
-                            <Text textTransform="uppercase">Título</Text>
-                        </Flex>
-                            <ContentMusic numb="1" music={"Cap Way"} artist={"Yunk Vino"} album={"237"} time={"2:56"}></ContentMusic>
-                    </Box>
+  return (
+    <>
+      <Flex>
+        <NavBarLeft />
+        <Box width="100%" maxHeight="500px" minHeight="340px">
+          <Box background="linear-gradient(transparent 0, rgba(0,0,0.5) 100%) #fff">
+            <Box
+              padding="86px 32px 24px"
+              max-width="100%"
+              backgroundColor="rgb(32, 72, 48)"
+            >
+              <Flex>
+                <DefaultImage
+                  src={"/assets/playlist/playlistImage.svg"}
+                  alt={"Imagem da playlist: o drip"}
+                />
+                <Box color="white" width="auto" margin="24px 0 0 24px">
+                  <Text textTransform="uppercase" paddingTop="10px">
+                    playlist
+                  </Text>
+                  <Heading margin="8px 0 12px" fontSize="98px" fontWeight="900">
+                    O Drip
+                  </Heading>
+                  <Text>
+                    Chique e confortável como a realeza do Trap. Foto: Brandão85
+                  </Text>
                 </Box>
+              </Flex>
+            </Box>
+          </Box>
+          <Box background="#121212" padding="32px 32px">
+            <Flex
+              padding="0 16px"
+              color="#b3b3b3"
+              marginBottom="32px"
+              gap="16px"
+              borderBottom="#fff"
+            >
+              <Text>#</Text>
+              <Text textTransform="uppercase">Título</Text>
             </Flex>
-        </>
-    );
+            <ContentMusic
+              numb="1"
+              music={"Cap Way"}
+              artist={"Yunk Vino"}
+              album={"237"}
+              time={"2:56"}
+            ></ContentMusic>
+          </Box>
+        </Box>
+      </Flex>
+    </>
+  );
 }
