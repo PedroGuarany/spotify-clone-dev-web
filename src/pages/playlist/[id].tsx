9976f99ae3774c121cfde6ba6b8cc26592196fb8
mwises <88902469+mwises@users.noreply.github.com>
2022-11-08 12:35:34 -0300

    express

--- src/pages/playlist/[id].tsx
+++ src/pages/playlist/[id].tsx
@@ -8,7 +8,7 @@ export default function Playlist() {
     
     return (
       <Box width="100%" height="100%" background={"#000"}>
-        <PlaylistComponent id={id}/>
+        <PlaylistComponent />
       </Box>
   );
 }
